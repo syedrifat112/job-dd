d5101df47ff7d803c8424b388cefecf133848de0
SyedRifat <sayedrifat112@gmail.com>
2023-04-11 21:20:03 +0600

    add category

---
+++ src/components/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div className='text-center my-16'>
+            <h1 className='font-bold text-3xl mb-4'>Job Category List</h1>
+            <p className='text-slate-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+    );
+};
+
+export default Category;
