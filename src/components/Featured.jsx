25edcecffd9e3d1eff824791687c2bab15b25544
SyedRifat <sayedrifat112@gmail.com>
2023-04-11 21:18:05 +0600

    add featured

---
+++ src/components/Featured.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <div className='text-center my-16'>
+        <h1 className='font-bold text-3xl mb-4'>Featured Jobs</h1>
+        <p className='text-slate-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+    </div>
+    );
+};
+
+export default Featured;
