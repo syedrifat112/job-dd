2fcdddb18d3f10c9431ea614e3a50ca3adffb8db
SyedRifat <sayedrifat112@gmail.com>
2023-04-11 21:22:46 +0600

    add blogs

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <div>
+                <h1>a. When should you use context API?</h1>
+                <p></p>
+            </div>
+            <div>
+                <h1>b.What is a custom hook?</h1>
+                <p></p>
+            </div>
+            <div>
+                <h1>c. What is useRef?</h1>
+                <p></p>
+            </div>
+            <div>
+                <h1>d. What is useMemo?</h1>
+                <p></p>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
